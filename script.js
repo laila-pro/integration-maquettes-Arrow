44688781ee16909c48bea6e91bb67ea27a355dce
LAILA <l.provost@codeur.online>
2019-09-16 11:27:39 +0200

    segment office

---
+++ script.js
@@ -0,0 +1,28 @@
+var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+	denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+	aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+    golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+
+            var cities = L.layerGroup([littleton, denver, aurora, golden]);
+
+  var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution}),
+	streets   = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
+
+var map = L.map('map', {
+	center: [39.73, -104.99],
+	zoom: 10,
+	layers: [grayscale, cities]
+});
+var baseMaps = {
+	"Grayscale": grayscale,
+	"Streets": streets
+};
+
+var overlayMaps = {
+    "Cities": cities
+};
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+var baseMaps = {
+	"<span style='color: gray'>Grayscale</span>": grayscale,
+	"Streets": streets
+};
